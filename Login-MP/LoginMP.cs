12e88df701be4c6f02f947e63b68899d3e7118e6
obarker1217 <obarker1217@ksu.edu>
2020-09-10 10:41:39 -0500

    Finished classwork

---
+++ Login-MP/LoginMP.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_MP
+{
+    public partial class LoginMP : Form
+    {
+        /// <summary>
+        /// The reference to the controller object, later
+        /// we need to replace this with delegate(s).
+        /// </summary>
+        InputHandler iHandler;
+
+        public LoginMP(InputHandler ih)
+        {
+            InitializeComponent();
+            this.iHandler = ih;
+        }
+
+        /// <summary>
+        /// Listener to the Login button. It takes the user's input
+        /// for the username and password and pass the values to the
+        /// Controller along with the state the view is in.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxLoginBtn_Click(object sender, EventArgs e)
+        {
+            String un = tbUserName.Text;
+            String up = tbPassword.Text;
+            Console.WriteLine(un + " " + up);
+            iHandler(State.GOTPASSWORD, un + ":" + up);
+        }
+
+        /// <summary>
+        /// THis method keepts the GUI controlls enabled/disabled, displaying the
+        /// right information based on the App's satate.
+        /// </summary>
+        /// <param name="state"></param>
+        public void DisplayState(State state)
+        {
+            switch (state)
+            {
+                case State.START:
+                    lbStateMessage.Text = "Please Enter Username";
+                    tbPassword.Enabled = false;
+                    uxLoginBtn.Enabled = false;
+                    break;
+                case State.GOTUSERNAME:
+                    lbStateMessage.Text = "Please Enter Password";
+                    tbPassword.Enabled = true;
+                    break;
+                case State.GOTPASSWORD:
+                    lbStateMessage.Text = "Validating Credentials...";
+                    break;
+                case State.DECLINED:
+                    tbUserName.Text = "";
+                    tbPassword.Text = "";
+                    lbStateMessage.Text = "Sorry, Invalid Credentials";
+                    break;
+                case State.SUCCESS:
+                    lbStateMessage.Text = "Congrats! You are Loggedin";
+                    break;
+                default:
+                    lbStateMessage.Text = "Invalid State";
+                    break;
+
+            }
+        }
+}
---
+++ Login-MP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_MP
+{
+    public delegate void InputHandler(State state, String args);
+    public delegate void StateObs(State state);
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();            
+            
+            CredentialsMP model = new CredentialsMP();
+            ControllerMP controller = new ControllerMP(model);
+            LoginMP view = new LoginMP(controller.handleEvents);
+            controller.registerObs(view.DisplayState);
+        }
+    }
+}
